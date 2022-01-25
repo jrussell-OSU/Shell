efc497049baa6f9a3ab20b555a202750549c8ed7
Jacob Russell <russelj2@os1.engr.oregonstate.edu>
2022-01-25 15:49:36 -0800

    able to get and fully parse a command from user. todo: do something with the commands

--- smallsh.c
+++ smallsh.c
@@ -21,17 +21,18 @@ struct CommandLine {
 
 char * get_command(char command_line[]);
 struct CommandLine *parse_command(char command_line[]);
+void print_command(struct CommandLine *curr_command);
 
 int main()
 {
-    char command_line[2100];
+    char command_line[3000];
 
     while(1){
         
-        get_command(command_line);
-        //printf("Your command: %s", command_line);  //for debugging
-        
-        parse_command(command_line);
+        get_command(command_line); //Get command line input from user        
+
+        struct CommandLine *curr_command = parse_command(command_line); //Parse command input, returns structure with parsed commands
+
         
     }
 
@@ -55,55 +56,99 @@ char * get_command(char command_line[])
     return command_line;
 }
 
-
+//Parse the command line input and put into a structure
 struct CommandLine *parse_command(char command_line[])
 {
     char *saveptr; //holds place for strtok_r
     char *token; //holds delim'd tokens from strtok_r
-    char args_array[1000];
+    int newline_flag = 0;  //if a token has a \n char
     
     struct CommandLine *curr_command = malloc(sizeof(struct CommandLine)); //create (allocate memory for) CommandLine structure
     
+    curr_command->cmd = NULL;
+    curr_command->arguments = NULL;
+    curr_command->input_file = NULL;
+    curr_command->output_file = NULL;
+    curr_command->background_flag = 0;
+    
     token = strtok_r(command_line, " ", &saveptr); //holds current delimn'd token from the command_line user input
+
+    //Check for # comments and blank lines
+    if (!(strcmp("\n", token)) || !(strncmp("#", token, 1))){
+        //printf("Comment or blank line ignored.\n"); //DEBUGGING
+        return curr_command;  //truncate function
+    }
 
     //Get "command" portion of user command (first delim'd token)
     curr_command->cmd = calloc(strlen(token) + 1, sizeof(char));
+
     strcpy(curr_command->cmd, token);
-    printf("Command from line is: %s\n", curr_command->cmd); // for debug
+
 
     //Get "arguments" (if any)
+    curr_command->arguments = calloc(1000, sizeof(char));  //allocate mem for arguments
     while (!strncmp("-", (token = strtok_r(NULL, " ", &saveptr)), 1)){
-        strcat(args_array, token);
-        //strcat(curr_command->arguments, token);
-        //printf("Arguments: %s\n", token);  //DEBUGGING
-    }
-    printf("Arguments: %s\n", args_array);  //DEBUGGING
-    //printf("Arguments: %s\n", curr_command->arguments);  //DEBUGGING
-
-    //Get "input_file" (if any)
-    if (!strcmp("<", token)){ //if it begins with "<" it's an input file
-        token = strtok_r(NULL, " ", &saveptr);
-        curr_command->input_file = calloc(strlen(token) + 1, sizeof(char));
-        strcpy(curr_command->input_file, token);
-        printf("Input file: %s\n", curr_command->input_file); // for debug
-        token = strtok_r(NULL, " ", &saveptr);
+        strcat(curr_command->arguments, token);
     }
 
-    //Get "output_file" (if any)
-    if (!strcmp(">", token)){ //if it begins with "<" it's an output file
-        token = strtok_r(NULL, " ", &saveptr);
-        curr_command->output_file = calloc(strlen(token) + 1, sizeof(char));
-        strcpy(curr_command->output_file, token);
-        printf("Output file: %s\n", curr_command->output_file); // for debug
+    //Get input and output files
+    while (saveptr && (!strcmp("<", token) || !strcmp(">", token))){
+        
+        //Get "input_file" (if any)
+        if (!strcmp("<", token) && curr_command->input_file == NULL){ //if it begins with "<" it's an input file
+            token = strtok_r(NULL, " ", &saveptr);
+            if (strstr(token, "\n")) //check for newline characters, that means we have reached end of input
+                newline_flag = 1;
+            curr_command->input_file = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(curr_command->input_file, strtok(token, "\n"));
+        }
+
+        //Get "output_file" (if any)
+        else if (!strcmp(">", token) && curr_command->output_file == NULL){ //if it begins with ">" it's an output file
+            token = strtok_r(NULL, " ", &saveptr);
+            if (strstr(token, "\n"))
+                newline_flag = 1;
+            curr_command->output_file = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(curr_command->output_file, strtok(token, "\n"));
+        } 
+        
+        //If more than one of the same > or < found, error
+        else {
+            printf("Error, too many files.\n");  //DEBUGGING
+            //print_command(curr_command);  //DEBUGGING
+            return curr_command;  //exit function early
+        }
+
+        //If there was a newline found, terminate function early to avoid segfaults, since we are at end of command anyway
+        if (newline_flag){
+            print_command(curr_command);  //DEBUGGING
+            return curr_command;
+        }
         token = strtok_r(NULL, " ", &saveptr);
     }
 
-    //Get if background or foreground process
-    if (!strncmp("&", token, 1))
-        printf("Background process: %s", token);
-    else
-        printf("Foreground process: %s", token);
-
-    return curr_command;
+    //Get if background or foreground process (defaults to foreground)
+    if (!strcmp("&\n", token))    
+        curr_command->background_flag = 1;  //background
+    
+    print_command(curr_command);  //DEBUGGING
+    fflush(stdout);
+    return curr_command; //return CommandLine structure
 }
 
+//Prints command_struct where members are not NULL. Used for DEBUGGING
+void print_command(struct CommandLine *curr_command)
+{
+    if (curr_command->cmd != NULL)
+        printf("Command: '%s'\n", curr_command->cmd);
+    if (curr_command->arguments != NULL)
+        printf("Arguments: '%s'\n", curr_command->arguments);
+    if (curr_command->input_file != NULL)
+        printf("Input file: '%s'\n", curr_command->input_file);
+    if (curr_command->output_file != NULL)
+        printf("Output file: '%s'\n", curr_command->output_file);
+    if (curr_command->background_flag)
+        printf("Background process\n");
+    else
+        printf("Foreground process\n");    
+}
