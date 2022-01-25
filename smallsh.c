7068b26b0731108e7c3d8185a73d8deb2b9b1723
Jacob Russell <russelj2@os1.engr.oregonstate.edu>
2022-01-24 19:14:40 -0800

    able to partly parse command line input into a struct

--- smallsh.c
+++ smallsh.c
@@ -12,31 +12,33 @@
 
 
 struct CommandLine {
-    char* command;    
-    int args_array[2100];
-    char* input_file;
-    char* output_file;
-    char* background_flag;
+    char * cmd;    
+    char * arguments;
+    char * input_file;
+    char * output_file;
+    int background_flag;
 };
 
-char * get_command(char command[]);
+char * get_command(char command_line[]);
+struct CommandLine *parse_command(char command_line[]);
 
 int main()
 {
-    char command[2100];
+    char command_line[2100];
 
     while(1){
         
-        get_command(command);
+        get_command(command_line);
+        //printf("Your command: %s", command_line);  //for debugging
         
-        printf("Your command: %s", command);  //for debugging
+        parse_command(command_line);
         
     }
 
     return 0;
 }
 
-char * get_command(char command[])
+char * get_command(char command_line[])
 {
     //Set up getline()
     char *curr_line = NULL;
@@ -47,7 +49,61 @@ char * get_command(char command[])
     putchar(':');
     getline(&curr_line, &buff_size, stdin);
 
-    strcpy(command, curr_line);
-
-    return command;
+    strcpy(command_line, curr_line);
+    
+    free(curr_line);
+    return command_line;
 }
+
+
+struct CommandLine *parse_command(char command_line[])
+{
+    char *saveptr; //holds place for strtok_r
+    char *token; //holds delim'd tokens from strtok_r
+    char args_array[1000];
+    
+    struct CommandLine *curr_command = malloc(sizeof(struct CommandLine)); //create (allocate memory for) CommandLine structure
+    
+    token = strtok_r(command_line, " ", &saveptr); //holds current delimn'd token from the command_line user input
+
+    //Get "command" portion of user command (first delim'd token)
+    curr_command->cmd = calloc(strlen(token) + 1, sizeof(char));
+    strcpy(curr_command->cmd, token);
+    printf("Command from line is: %s\n", curr_command->cmd); // for debug
+
+    //Get "arguments" (if any)
+    while (!strncmp("-", (token = strtok_r(NULL, " ", &saveptr)), 1)){
+        strcat(args_array, token);
+        //strcat(curr_command->arguments, token);
+        //printf("Arguments: %s\n", token);  //DEBUGGING
+    }
+    printf("Arguments: %s\n", args_array);  //DEBUGGING
+    //printf("Arguments: %s\n", curr_command->arguments);  //DEBUGGING
+
+    //Get "input_file" (if any)
+    if (!strcmp("<", token)){ //if it begins with "<" it's an input file
+        token = strtok_r(NULL, " ", &saveptr);
+        curr_command->input_file = calloc(strlen(token) + 1, sizeof(char));
+        strcpy(curr_command->input_file, token);
+        printf("Input file: %s\n", curr_command->input_file); // for debug
+        token = strtok_r(NULL, " ", &saveptr);
+    }
+
+    //Get "output_file" (if any)
+    if (!strcmp(">", token)){ //if it begins with "<" it's an output file
+        token = strtok_r(NULL, " ", &saveptr);
+        curr_command->output_file = calloc(strlen(token) + 1, sizeof(char));
+        strcpy(curr_command->output_file, token);
+        printf("Output file: %s\n", curr_command->output_file); // for debug
+        token = strtok_r(NULL, " ", &saveptr);
+    }
+
+    //Get if background or foreground process
+    if (!strncmp("&", token, 1))
+        printf("Background process: %s", token);
+    else
+        printf("Foreground process: %s", token);
+
+    return curr_command;
+}
+
